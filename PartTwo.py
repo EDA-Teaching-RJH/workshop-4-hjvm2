c8b10db7e8c81c7048e6bafb3641b4026d1aadcb
hjvm2 <hjvm2@kent.ac.uk>
2024-10-18 11:44:28 +0000

    something

--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,23 @@
+def main():
+    choice = int(input("1. Coffee, 2. Tea, 3. Hot Chocolate: "))
+    coin_inserted = 0
+    
+    if choice == '1':
+        price = 75
+        print("Invalid choice")
+        return
+    
+    print(f"The price is", {price})
+    
+    while coin_inserted < price:
+        coin = int(input("Insert coins (50p,20p,10p,5p): "))
+        coin_inserted += coin
+        print(f"Total inserted:", {coin_inserted})
+        
+        if coin_inserted < price:
+            print(f"Amount Due: ", {price - coin_inserted})
+    
+    print("Thanks for your purchase!")
+
+main()
 
