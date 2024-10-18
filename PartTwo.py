41cbac903259c4df966486284ec086a7be9ca172
hjvm2 <hjvm2@kent.ac.uk>
2024-10-18 11:54:13 +0000

    1

--- PartTwo.py
+++ PartTwo.py
@@ -1,23 +1,36 @@
 def main():
-    choice = int(input("1. Coffee, 2. Tea, 3. Hot Chocolate: "))
-    coin_inserted = 0
+    choice = input("1. Coffee, 2. Tea, 3. Hot Chocolate: ")
+    coininserted = 0
+    price = 0
     
     if choice == '1':
         price = 75
+    elif choice == '2':
+        price = 60  
+    elif choice == '3':
+        price = 80  
+    else:
         print("Invalid choice")
         return
     
-    print(f"The price is", {price})
+    print(f"The price is {price} pence.")
     
-    while coin_inserted < price:
-        coin = int(input("Insert coins (50p,20p,10p,5p): "))
-        coin_inserted += coin
-        print(f"Total inserted:", {coin_inserted})
+    while coininserted < price:
+        coin = int(input("Insert coins (50p, 20p, 10p, 5p): "))
+        coininserted += coin
+        print(f"Total inserted: {coininserted} pence.")
         
-        if coin_inserted < price:
-            print(f"Amount Due: ", {price - coin_inserted})
+        if coininserted < price:
+            print(f"Amount due: {price - coininserted} pence.")
+        
+        elif coininserted > price:
+            minustopos = abs {price - conininserted}
+            print(f"Change due: {price - coininserted} pence.")
+        else:
+            pass
+
+
     
     print("Thanks for your purchase!")
 
 main()
-
