0273f3fbc180572cf4edcc820050b2aef8b1abc3
hjvm2 <hjvm2@kent.ac.uk>
2024-10-18 11:44:07 +0000

    1

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,13 @@
+def cameltosnake(input):
+    output = ""
+    for char in input:
+        if char.isupper():
+            output+= "_" + char.lower()
+        else:
+            output += char
+    return output.lstrip('_')
+
+input = input("Enter camel case: ")
+result = cameltosnake(input)
+print("snake case conversion:", result)
 
